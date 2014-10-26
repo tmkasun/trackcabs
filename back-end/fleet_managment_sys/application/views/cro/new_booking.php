e4362b1b3ecac2ccef331fdc8ab08d391cbbc7a2
Dehan <dehandecroos@gmail.com>
2014-10-26 09:33:50 -0700

    UI Modified for adding new order

--- back-end/fleet_managment_sys/application/views/cro/new_booking.php
+++ back-end/fleet_managment_sys/application/views/cro/new_booking.php
@@ -1,127 +1,150 @@
-<div class="col-lg-5">
-    <form role="form">
-        <div class="form-group">
-            <input type="text" class="form-control" id="no" placeholder="Number" >
-        </div>
-        <div class="form-group">
-            <input type="text" class="form-control" id="road" placeholder="Road" >
-        </div>
-        <div class="form-group">
-            <input type="text" class="form-control" id="city" placeholder="City">
-        </div>
-        <div class="form-group">
-            <input type="text" class="form-control" id="town" placeholder="Town">
-        </div>
-
-        <div class="form-group">
-            <input type="text" class="form-control" id="landMark" placeholder="Land Mark">
-        </div>
-
-        <div class="form-group">
-            <input type="text" class="form-control" id="remark" placeholder="Remark">
-        </div>
-
-        <div class="form-group">
-            <input type="text" class="form-control" id="dispatchB4" placeholder="Dispatch Before">
-        </div>
-
-        <div class="form-group">
-            <div class="checkbox">
-                <label>
-                    <input type="checkbox" id="callUp" > Call Up
-                </label>
+<form id="newBookingForm">
+    <div class="col-lg-5">
+        <div class="panel panel-success">
+            <div class="panel-heading">
+                <h3 class="panel-title">Location Details</h3>
             </div>
-            <input type="text" class="form-control" id="callUpPrice" placeholder="Call Up Price" >
+
+            <div class="panel-body">
+                <div class="form-group">
+                    <label class="control-label">Number</label>
+                    <input class="form-control" type="text"  id="no"      name="no"      placeholder="Number" >
+                </div>
+                <div class="form-group">
+                    <label>Road</label>
+                    <input type="text" class="form-control" id="road"     name="road"     placeholder="Road" >
+                </div>
+                <div class="form-group">
+                    <label>City</label>
+                    <input type="text" class="form-control" id="city"     name="city"     placeholder="City">
+                </div>
+                <div class="form-group">
+                    <label>Town</label>
+                    <input type="text" class="form-control" id="town"     name="town"     placeholder="Town">
+                </div>
+                <div class="form-group">
+                    <label>Land Mark</label>
+                    <input type="text" class="form-control" id="landMark" name="landMark" placeholder="Land Mark">
+                </div>
+            </div>
+
         </div>
 
+        <div class="panel panel-success" style="margin-top:2%">
+            <div class="panel-heading">
+                <h3 class="panel-title">Other Dispatch Details</h3>
+            </div>
 
+            <div class="panel-body">
+                <div class="form-group">
+                    <label>Remark</label>
+                    <input type="text" class="form-control" id="remark"       name="remark"    placeholder="Remark">
+                </div>
+                <div class="form-group">
+                    <label>Dispatch Before<small style="font-weight: lighter"> [In Minutes]</small></label>
+                    <input type="text" class="form-control" id="dispatchB4"   name="dispatchBefore"   value="30" placeholder="Dispatch Before">
+                </div>
 
-    </form>
-</div>
-
-
-<div class="col-lg-3" style="border-left: 2px solid #a6a6a6" >
-
-    <div class="form-group">
-        <label for="dtp_input3" class="control-label">Booking Time</label>
-        <div id="form_time" class="input-group date" data-date="" data-date-format="hh:ii" data-link-field="dtp_input3" data-link-format="hh:ii">
-            <input id="bTime" class="form-control" size="16" type="text" value="" readonly>
-            <span class="input-group-addon"><span class="glyphicon glyphicon-remove"></span></span>
-            <span class="input-group-addon"><span class="glyphicon glyphicon-time" onclick="showCalender()"></span></span>
-        </div>
-        <input type="hidden" id="dtp_input3" value="" /><br/>
-    </div>
-
-    <div class="form-group">
-        <label for="dtp_input2" class="control-label">Booking Date</label>
-        <div id="form_date" class="input-group date" data-date="" data-date-format="yyyy-mm-dd" data-link-field="dtp_input2" data-link-format="yyyy-mm-dd">
-            <input id="bDate" class="form-control" size="16" type="text" value="" readonly>
-            <span class="input-group-addon"><span class="glyphicon glyphicon-remove"></span></span>
-            <span class="input-group-addon"><span class="glyphicon glyphicon-calendar" onclick="showCalender()"></span></span>
-        </div>
-        <input type="hidden" id="dtp_input2" value="" /><br/>
-    </div>
-
-
-    <h4>Classification</h4>
-    <label class="checkbox-inline">
-        <input type="checkbox" id="inlineCheckbox1" value="option1"> UN Mark
-    </label>
-    <label class="checkbox-inline">
-        <input type="checkbox" id="inlineCheckbox2" value="option2"> Tinted
-    </label>
-    <label class="checkbox-inline">
-        <input type="checkbox" id="inlineCheckbox3" value="option3"> VIP
-    </label></br>
-    <label>
-        <input type="checkbox"  value="vih">
-        VIH&mdash;Dr.Appointment, Court Case, Interview
-    </label></br>
-
-    <label>
-        <input type="checkbox"  value="noNotSend">
-        Don't Send Customer Mobile Number
-    </label>
-
-
-
-</div>
-
-<div class="col-lg-2" style="border-left: 2px solid #a6a6a6">
-    <div class="form-group">
-        <h5>Choose Vehicle Type</h5>
-        <label class="radio-inline">
-            <input type="radio" id="carRadio" name="vehicleRadio" value="car"> Car
-        </label>
-        <label class="radio-inline">
-            <input type="radio" id="vanRadio" name="vehicleRadio" value="van"> Van
-        </label>
-        <label class="radio-inline">
-            <input type="radio" id="nanoRadio" name="vehicleRadio" value="nano"> Nano
-        </label>
-    </div>
-
-    <div class="form-group">
-        <h5>Payment Type</h5>
-        <label class="radio-inline">
-            <input type="radio" id="cashRadio" name="paymentRadio" value="cash"> Cash
-        </label>
-        <label class="radio-inline">
-            <input type="radio" id="creditRadio" name="paymentRadio" value="credit"> Credit
-        </label>
-    </div>
-
-</div>
-
-<div class="col-lg-2">
-
-    <div class="btn-group btn-group-justified">
-        <div class="btn-group">
-            <button type="button" class="btn btn-success" onclick="operations('createBooking')">Save Booking</button>
+                <div class="form-group">
+                    <div class="checkbox">
+                        <label style="font-weight: bold"><input type="checkbox" name="callUp"  id="callUp"> Call Up</label>
+                    </div>
+                    <input type="text" class="form-control" id="callUpPrice" name="callUpPrice" placeholder="Call Up Price" style="display:none">
+                </div>
+            </div>
         </div>
     </div>
-</div>
+    <div class="col-lg-5" style="border-left: 2px solid #eee" >
+        <div class="panel panel-success">
+            <div class="panel-heading">
+                <h3 class="panel-title">Booking Details</h3>
+            </div>
 
+            <div class="panel-body">
+
+                <div class="form-group">
+                    <label class="control-label" style="font-weight:bold">Vehicle Type</label></br>
+                    <div class="btn-group">
+                        <button type="button" id="carRadio"  data-set="vehicle" value="car" class="btn btn-default customRadio">Car</button>
+                        <button type="button" id="vanRadio"  data-set="vehicle" value="van" class="btn btn-default customRadio">Van</button>
+                        <button type="button" id="nanoRadio"   data-set="vehicle" value="nano" class="btn btn-default customRadio">Nano</button>
+                    </div>
+                    <input style="display: none" class="customRadio" name="vehicleType" id="vehicleType">
+
+                </div>
+
+                <div class="form-group">
+                    <label class="control-label" style="font-weight:bold">Payment Type</label></br>
+                    <div class="btn-group">
+                        <button type="button" data-set="payment" value="cash " class="btn btn-default customRadio">Cash</button>
+                        <button type="button" data-set="payment" value="credit" class="btn btn-default customRadio">Credit Card</button>
+                    </div>
+                    <input style="display: none" class="customRadio" name="paymentType" id="paymentType">
+                </div>
+
+                <div class="form-group">
+                    <label class="control-label">Booking Time</label>
+                    <div id="form_time" class="input-group date" data-date="" data-date-format="hh:ii" data-link-field="dtp_input3" data-link-format="hh:ii">
+                        <input id="bTime" name="bTime" class="form-control" name= size="16" type="text" value="" readonly="readonly">
+                        <span class="input-group-addon"><span class="glyphicon glyphicon-time" onclick="showCalender()"></span></span>
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <label class="control-label">Booking Date</label>
+                    <div id="form_date" class="input-group date" data-date="" data-date-format="yyyy-mm-dd" data-link-field="dtp_input2" data-link-format="yyyy-mm-dd">
+                        <input id="bDate" name="bDate" class="form-control" size="16" type="text" value="" readonly="readonly">
+                        <span class="input-group-addon"><span class="glyphicon glyphicon-calendar" onclick="showCalender()"></span></span>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="panel panel-success" style="margin-top:2%">
+            <div class="panel-heading">
+                <h3 class="panel-title">Booking Requirements</h3>
+            </div>
+            <div class="panel-body">
+                <div class="form-group">
+                    <div class="checkbox">
+                        <label><input type="checkbox" id="unmarked" >Unmarked</label>
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <div class="checkbox">
+                        <label><input type="checkbox" id="tinted"       >Tinted</label>
+                    </div>
+                </div>
+
+
+                <div class="form-group">
+                    <div class="checkbox">
+                        <label><input type="checkbox" id="vip"          >VIP</label>
+                    </div>
+                </div>
+
+
+                <div class="form-group">
+                    <div class="checkbox">
+                        <label><input type="checkbox" id="vih"      >Very Important Hire<small style="font-weight: lighter"> [Court Case/Interview/Appointment]</small></label>
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <div class="checkbox">
+                        <label><input type="checkbox" id="cusNumberNotSent">Don't Send Customer Number</label>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-2">
+        <div class="form-group" style="text-align: center">
+            <button type="submit" style="width:100%" class="btn btn-success" >Save Booking</button>
+        </div>
+    </div>
+</form>
 
 
 

7b7cd9942e5abe33e23481fb2b77c0c537a70960
drifftr <drifftr@gmail.com>
2014-11-07 14:09:20 +0530

    Added user login block validation

--- back-end/fleet_managment_sys/application/controllers/login.php
+++ back-end/fleet_managment_sys/application/controllers/login.php
@@ -39,15 +39,16 @@ class Login extends CI_Controller {
         $userName = $this->input->post('username');
         $pass = $this->input->post('password');
         $result = $this->user_dao->authenticate($userName,$pass);
-        var_dump($result);
         if($result != null ){
-            if($result['blocked'] != 'true'){
+            if($result["blocked"] == 'true'){
+                $this->session->set_userdata('blocked', true);
+              }else{
                 $this->session->set_userdata('logged_in', true);
                 $this->session->set_userdata('user', $result);
-              }else{
-                $this->session->set_userdata('blocked', true);
-            }
+              }
 
+        }else{
+            $this->session->set_userdata('blocked', false);
         }
 
         $this->index();
--- back-end/fleet_managment_sys/application/helpers/authentication_helper.php
+++ back-end/fleet_managment_sys/application/helpers/authentication_helper.php
@@ -8,18 +8,26 @@ if (!function_exists('is_user_logged_in')) {
         //return true; // TODO: remove this line, until get the database authentication support
 
         $ci =& get_instance();
-        if ($ci->session->userdata('blocked')) {
-            $ci->session->set_flashdata('error', 'You account has been blocked.Please contact Admin !');
+        if($ci->session->userdata('blocked')){
+            $ci->session->set_userdata('error', 'You account has been blocked.Please contact Admin !');
+            var_dump("Blocked Account helper");
             return FALSE;
-        } else {
+        }else {
+
+
             if ($ci->session->userdata('logged_in')) {
+
                 $session_data = $ci->session->userdata('logged_in');
                 return $session_data;
+
             } else {
-                $ci->session->set_flashdata('error', 'Invalid username or password!');
+                $ci->session->set_userdata('error', 'Invalid username or password!');
+                var_dump("Login Fail");
                 return FALSE;
             }
         }
+
+
     }
 
 }

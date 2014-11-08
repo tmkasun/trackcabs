69d13e86700018268a89208d94d147810455dac9
Dehan <dehandecroos@gmail.com>
2014-11-08 09:48:03 +0530

    added the button to add user

--- back-end/fleet_managment_sys/application/views/cro/customer_info.php
+++ back-end/fleet_managment_sys/application/views/cro/customer_info.php
@@ -1,4 +1,4 @@
-<div class="panel panel-default">
+<div class="panel panel-default" xmlns="http://www.w3.org/1999/html">
     <div class="panel-heading">
         <h3 class="panel-title">Customer Information</h3>
     </div>
@@ -24,21 +24,48 @@
                     </tr>
                 </table>
             </div>
-            <div class="col-lg-offset-8 col-lg-4" >
-                <div class="btn-group btn-group-justified">
-                    <div class="btn-group">
-                        <button type="button" class="btn btn-default" onclick="operations('editCus', '<?= $tp;?>' )">Edit Info</button>
-                    </div>
+            <div class="col-lg-12" >
+                <div class="col-lg-offset-7 col-lg-3">
                     <?php if($profileType == 'Cooperate'):?>
-                        <div class="btn-group">
-                            <button type="button" class="btn btn-default" onclick="operations('addCustomer', '<?= $tp;?>' )">Add User</button>
-                        </div>
+                    <div class="input-group">
+                        <input type="text" class="form-control">
+                              <span class="input-group-btn">
+                                <button class="btn btn-default" type="button">Add User</button>
+                              </span>
+                    </div><!-- /input-group -->
                     <?php endif;?>
+                </div>
+                <div class="col-lg-2">
+                    <div class="btn-group btn-group-justified">
+                        <div class="btn-group">
+                            <button type="button" class="btn btn-default" onclick="operations('editCus', '<?= $tp;?>' )">Edit Info</button>
+                        </div>
+                    </div>
                 </div>
             </div>
 
             <div class="col-lg-12">
-                
+                <?php if(isset($userInfo)):?>
+
+                    <table class="table table-striped" >
+                        <tr>
+                            <th>Title</th>
+                            <th>Position</th>
+                            <th>Name</th>
+                            <th>Permanent Remarks</th>
+                            <th>Organization</th>
+                        </tr>
+
+                        <?php foreach($userInfo as $item)?>
+                        <tr>
+                            <td><?= $item['title'];?></td>
+                            <td><?= $item['position'];?></td>
+                            <td><?= $item['name'];?></td>
+                            <td><?= $item['pRemark'];?></td>
+                            <td><?= $item['org'];?></td>
+                        </tr>
+                    </table>
+                <?php endif;?>
             </div>
         </div>
 </div>

d748293b7632106348392360b03ef732757a26d4
Dehan <dehandecroos@gmail.com>
2014-11-09 11:52:22 +0530

    added logout for the admin navbar

--- back-end/fleet_managment_sys/application/views/admin/manage_cabs.php
+++ back-end/fleet_managment_sys/application/views/admin/manage_cabs.php
@@ -51,15 +51,6 @@
             </form>
             <ul class="nav navbar-nav navbar-right">
                 <li><a href="<?= site_url('login/logout')?>">Log Out</a></li>
-                <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
-                    <ul class="dropdown-menu">
-                        <li><a href="#">Action</a></li>
-                        <li><a href="#">Another action</a></li>
-                        <li><a href="#">Something else here</a></li>
-                        <li><a href="#">Separated link</a></li>
-                    </ul>
-                </li>
             </ul>
         </div><!-- /.navbar-collapse -->
     </nav>

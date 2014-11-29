4993877121709e2ef9b8a295ed0433c1640ad58e
Dehan <dehandecroos@gmail.com>
2014-11-29 14:29:08 +0530

    changes

--- back-end/fleet_managment_sys/application/views/cro/map/map_main.php
+++ back-end/fleet_managment_sys/application/views/cro/map/map_main.php
@@ -36,7 +36,6 @@
             <li><a href="<?= site_url('cro_controller/loadBookingsView')?>" >Bookings</a></li>
             <li class="active"><a href="<?= site_url('cro_controller/loadMapView')?>" >Map</a></li>
             <li><a href="<?= site_url('cro_controller/loadLocationBoardView')?>" >Location Board</a></li>
-            <li><a href="<?= site_url('cro_controller/refresh')?>" >Refresh</a></li>
             <li><a href="<?= site_url('cro_controller/getCabHeaderView')?>" >Cabs</a>
             <li><a href="<?= site_url('cro_controller/getAllPackagesView')?>" >Packages</a>    
         </ul>

39ad76f1b5691b8c96779ebdb801b1cc4a39cbcd
AdbMora <adheeb91@gmail.com>
2014-11-29 07:53:17 +0530

    Cab search view changed to table, 'Edit' button re sized in user search views

--- back-end/fleet_managment_sys/application/views/admin/dispatcher/dispatcher_search.php
+++ back-end/fleet_managment_sys/application/views/admin/dispatcher/dispatcher_search.php
@@ -11,7 +11,7 @@
                         <th>tp</th>
                         <th>User Type</th>
                         <th>Blocked</th>
-                        <th>Action</th>
+<!--                        <th>Action</th>-->
                     </tr>
 
                     <?php if(isset($userId)){ ?>
@@ -24,12 +24,7 @@
                             <td><?= $tp;?></td>
                             <td><?= $user_type;?></td>
                             <td><?= $blocked; ?></td>
-                            <td><div class="btn-group btn-group-justified">
-                                    <div class="btn-group">
-                                        <button type="button" class="btn btn-success" onclick="makeCROFormEditable(<?= $userId;?>,url, '<?php echo $user_type;?>')">Edit</button>
-                                    </div>
-                                </div>
-                            </td>
+                            <td><button type="button" class="btn btn-success" onclick="makeCROFormEditable(<?= $userId;?>,url, '<?php echo $user_type;?>')">Edit</button></td>
                         </tr>
                     <?php } ?>
                 </table>

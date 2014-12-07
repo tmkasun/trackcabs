c020bfdf3605b6f3f21b762ef1254f526edc6b2e
Kasun Thennakoon <tmkasun@gmail.com>
2014-12-07 20:02:13 +0530

    add login notice typo

--- back-end/fleet_managment_sys/application/views/tracking/modals/login_message.php
+++ back-end/fleet_managment_sys/application/views/tracking/modals/login_message.php
@@ -18,7 +18,7 @@
                 The author(s) of this product is in no way responsible if the product makes any damage to your system or server, if it doesn't work as intended or even if it eats your dog.
 
                 It comes without any warranty, to the extent permitted by applicable law. You can not redistribute it and/or modify without proper permission of <span class="text-info"> Nitrous Technologies Lanka Pvt</span>.
-                This copy of vehicle tracking system is can only used by <span class="text-info">Hao City Cabs Lanka Pvt </span> within its internal network(Intranet).
+                This copy of vehicle tracking system can only be used by <span class="text-info">Hao City Cabs Lanka Pvt </span> within its internal network(Intranet).
 
             </p>
 

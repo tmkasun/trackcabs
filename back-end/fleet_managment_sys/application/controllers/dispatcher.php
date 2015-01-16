7493578710b305bcc2e99479d49f26dd80259e7d
Kasun Thennakoon <tmkasun@gmail.com>
2015-01-16 18:23:13 +0530

    Update pagination

--- back-end/fleet_managment_sys/application/controllers/dispatcher.php
+++ back-end/fleet_managment_sys/application/controllers/dispatcher.php
@@ -378,23 +378,28 @@ Booking cancelled. Do not proceed to hire. Sorry for the inconvenience.
         $this->load->view('dispatcher/modals/search_cab', array('cabs' => $allCabs));
     }
 
-    function dispatch_history($page=0)
+    function dispatch_history($page = 0)
     {
         $history = $this->input->get('history');
         $this->load->library('pagination');
-        $this->pagination->per_page = 10;
+        $this->pagination->per_page = 2;
         $this->pagination->uri_segment = 3;
 
-        if($history){
-            $this->pagination->base_url = base_url() . "index.php/dispatcher/dispatch_history?history=".$history;
-            $this->pagination->total_rows =  count($this->history_dao->getBookings(null,null,$history));
-        }
-        else{
+        if ($history) {
+            $spited_values = explode('/', $history);
+            if (count($spited_values) > 1) {
+                $page = $spited_values[1];
+            }
+            $history = $spited_values[0];
+            $this->pagination->base_url = base_url() . "index.php/dispatcher/dispatch_history?history=" . $history;
+            $this->pagination->total_rows = count($this->history_dao->getBookings(null, null, $history));
+        } else {
             $this->pagination->base_url = base_url() . "index.php/dispatcher/dispatch_history";
             $this->pagination->total_rows = $this->history_dao->bookingsCount();
         }
-        $history_booking = $this->history_dao->getBookings($this->pagination->per_page,(int)$page,$history);
-        $links =  $this->pagination->create_links();
+
+        $history_booking = $this->history_dao->getBookings($this->pagination->per_page, (int)$page, $history);
+        $links = $this->pagination->create_links();
         $this->load->view('dispatcher/modals/dispatch_history', array('history_booking' => $history_booking, 'links' => $links));
     }
 

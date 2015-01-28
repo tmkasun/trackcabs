1a9f7f384bc22fe798869062f3317cd2db1b1460
drifftr <drifftr@gmail.com>
2015-01-28 23:00:27 +0530

    added working hours testing 9

--- back-end/fleet_managment_sys/application/views/admin/reports/working_hours_table.php
+++ back-end/fleet_managment_sys/application/views/admin/reports/working_hours_table.php
@@ -14,14 +14,14 @@
         <tr>
             <td><?= $item['userId'];?></td>
             <td><?php $timeIn=$item['time'];
-                echo date('y-m-d , h:i:s', $timeIn->sec);
+                echo date('yyyy-m-d , h:i:s', $timeIn->sec);
                 ?></td>
             <td><?php $timeOut=$item['logout_time'];
                 if($timeOut=='-'){
-                    $timeOut = new MongoDate();
+                   // $timeOut = new MongoDate();
                     echo 'not logged out';
                 }else{
-                    echo date('y-m-d , h:i:s', $timeOut->sec);
+                    echo date('yyyyy-m-d , h:i:s', $timeOut->sec);
                 }
                 ?></td>
             <td><?php
@@ -29,12 +29,12 @@
                     echo 'not logged out';
                 }else{
                     $preWorkingHours = $timeOut->sec - $timeIn->sec;
-                    $workingHour=$preWorkingHours/3600;
-                    /*if($workingHour<24){*/
+                    $workingHour=(int)($preWorkingHours/3600);
+                    if($workingHour<24){
                         echo $workingHour."hours";
-                    /*}else{
-                        echo ($workingHour/24)." days ".($workingHour%24)." hours";
-                    }*/
+                    }else{
+                        echo (int)($workingHour/24)." days ".($workingHour%24)." hours";
+                    }
                 }
 
                 ?></td>

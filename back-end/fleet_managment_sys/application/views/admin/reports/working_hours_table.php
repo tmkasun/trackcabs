8f7aa74683e3dd4f606c542d2457d7eb8bc5ff30
drifftr <drifftr@gmail.com>
2015-01-28 22:09:06 +0530

    added working hours testing 6

--- back-end/fleet_managment_sys/application/views/admin/reports/working_hours_table.php
+++ back-end/fleet_managment_sys/application/views/admin/reports/working_hours_table.php
@@ -13,24 +13,7 @@
 
         <tr>
             <td><?= $item['driverId'];?></td>
-            <td><?php $timeIn=$item['time'];
-                echo date('h:i:s', $timeIn->sec);
-                ?></td>
-            <td><?php $timeOut=$item['logout_time'];
-                if($timeOut=='-'){
-                    $timeOut = new MongoDate();
-                    echo 'not logged out';
-                }else{
-                    echo date('h:i:s', $timeOut->sec);
-                }
-                ?></td>
-            <td><?php $workingHour=$item['workingHours']/3600;
-                    if($workingHour<24){
-                        echo $workingHour."hours";
-                    }else{
-                        echo ($workingHour/24)." days ".($workingHour%24)." hours";
-                    }
-                ?></td>
+            
 
         </tr>
 

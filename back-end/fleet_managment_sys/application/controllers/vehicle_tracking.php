ccef577edb6528c5335d1eac7432912508edfe1c
Kasun Thennakoon <tmkasun@gmail.com>
2015-01-28 08:38:05 +0530

    Add data stat viewer

--- back-end/fleet_managment_sys/application/controllers/vehicle_tracking.php
+++ back-end/fleet_managment_sys/application/controllers/vehicle_tracking.php
@@ -12,4 +12,12 @@ class Vehicle_tracking extends CI_Controller
     function login_message(){
         $this->load->view('tracking/modals/login_message');
     }
+
+    function bigdata(){
+        $this->load->model('bigdata_dao');
+        $data_stats = $this->bigdata_dao->stats();
+        $this->load->view('bigdata/index',array('data_stats' => $data_stats));
+//        $this->output->set_content_type('application/json');
+//        echo $data_stats;
+    }
 }

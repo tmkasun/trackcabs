8a493f2c4ad55ebfc3c4178ba17986dc78e00415
AdbMora <adheeb91@gmail.com>
2015-02-05 14:40:54 +0530

    Search filter for complaints only for the complaint view

--- back-end/fleet_managment_sys/application/views/admin/reports/complaint_reports_view.php
+++ back-end/fleet_managment_sys/application/views/admin/reports/complaint_reports_view.php
@@ -1,4 +1,21 @@
+
+<div class="collapse navbar-collapse navbar-ex1-collapse">
+    <form class="navbar-form navbar-left" role="search" id="getAccount">
+            <div class="form-group" id="search_box_for_reports">
+                <input class="form-control" placeholder="Enter ID" type="text" id="idForSearch">
+            </div>
+            
+            <button type="submit" class="btn btn-default" onclick="getReportViewFromSearchId();return false;">Submit</button>
+            <select id="search_select">
+                <option value="driverId">Search by Driver ID</option>
+                <option value="refId">Search by Booking Ref ID</option>
+                <option value="complaintId">Search by Complaint ID</option>
+            </select>
+        </form>
+
 <table class="table table-striped" >
+    
+    
     <tr>
         <th>Complaint Ref ID</th>
         <th>Booking Ref ID</th>
